921dd42c20608385a603fa36a03f6540e77582e0
ahmadkgt <33150668+ahmadkgt@users.noreply.github.com>
2023-04-05 14:13:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose")
 
 mongoose.connect("mongodb+srv://ahmadkgt:vpfAv7j0FUsdypkG@cluster0.iuq0el8.mongodb.net/mern?retryWrites=true&w=majority")
 
-const UserModel = require('./Users.js')
+const UserModel = require('./Users')
 
 app.get("/users", async (req, res)=>{
   const user = await UserModel.find();
